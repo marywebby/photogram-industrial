0a7f85bd0224d16b715ba1132549cbcc9da0a340
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-03-04 18:17:51 +0000

    Generated photos

--- db/migrate/20240304173549_devise_create_users.rb
+++ db/migrate/20240304173549_devise_create_users.rb
@@ -3,7 +3,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      enable_extension("citext")
+      enable_extension("citext") 
 
       ## Database authenticatable
       t.citext :email,              null: false, default: ""
