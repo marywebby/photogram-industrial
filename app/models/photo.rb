6735a8472722b072abde402cd2b19ada278bee38
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-03-04 22:52:44 +0000

    added scopes to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -23,8 +23,12 @@ class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
   has_many :comments
   has_many :likes
-  has_many :fans, through: :likes, source: :fan
+  has_many :fans, through: :likes, source: :
+  
   validates :caption, presence: true
   validates :image, presence: true
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 
 end
