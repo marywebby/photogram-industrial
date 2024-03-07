22a7a566a81f804bf1ea72fcf8a639e6dce9924c
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-03-07 23:02:15 +0000

    tabs added

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
   get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#follower", as: :followers
+  get ":username/following" => "users#following", as: :following
+
   get "/:username" => "users#show", as: :user
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
+ 
